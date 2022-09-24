ef0e4992fc014662ab2259f005af669e18da13ff
Marco Gael <marcotecchi2@gmail.com>
2022-09-24 12:03:54 -0500

    changes: finish

--- script.js
+++ script.js
@@ -7,4 +7,46 @@ menuHD.addEventListener('click', function(event){
     menuMobile.classList.toggle('show')
 })
 
-console.log(menuHD)
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider-section");
+let sliderSectionLast = sliderSection[sliderSection.length - 1];
+
+const btnLetf = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement("afterbegin", sliderSectionLast)
+
+function Next() {
+    let sliderSectionFirst = document.querySelectorAll(".slider-section")[0];
+    slider.style.marginLeft = "-100%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+        slider.marginLeft = "-100%";
+    }, 500);
+}
+
+function Prev() {
+    let sliderSection = document.querySelectorAll(".slider-section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("afterbegin", sliderSectionLast)
+        slider.style.marginLeft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener("click", function(){
+    Next();
+});
+
+btnLetf.addEventListener("click", function(){
+    Prev();
+});
+
+// setInterval(function(){
+//     Next();
+// }, 5000);
